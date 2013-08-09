542c1a0fafa8f83cf4bdb326fbc9e17c2ef048e1
James Tauber <jtauber@jtauber.com>
2013-08-09 00:15:12 -0400

    implemented ref

--- tuples.py
+++ tuples.py
@@ -4,6 +4,9 @@
 class Tuple:
     def __init__(self, *components):
         self._components = components
+    
+    def __getitem__(self, index):
+        return self._components[index]
 
 
 class UpTuple(Tuple):
@@ -18,7 +21,16 @@ up = UpTuple
 down = DownTuple
 
 
+def ref(tup, *indices):
+    if indices:
+        return ref(tup[indices[0]], *indices[1:])
+    else:
+        return tup
+
+
 if __name__ == "__main__":
     v = up("v^0", "v^1", "v^2")
     p = down("p_0", "p_1", "p_2")
     s = up("t", up("x", "y"), down("p_x", "p_y"))
+    assert ref(up("a", "b", "c"), 1) == "b"
+    assert ref(up(up("a", "b"), up("c", "d")), 0, 1) == "b"
