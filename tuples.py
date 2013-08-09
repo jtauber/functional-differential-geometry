25afa197897ade888d81586572daeb5eaf8a8f82
James Tauber <jtauber@jtauber.com>
2013-08-09 00:18:04 -0400

    implemented component

--- tuples.py
+++ tuples.py
@@ -28,9 +28,16 @@ def ref(tup, *indices):
         return tup
 
 
+def component(*indices):
+    def _(tup):
+        return ref(tup, *indices)
+    return _
+
+
 if __name__ == "__main__":
     v = up("v^0", "v^1", "v^2")
     p = down("p_0", "p_1", "p_2")
     s = up("t", up("x", "y"), down("p_x", "p_y"))
     assert ref(up("a", "b", "c"), 1) == "b"
     assert ref(up(up("a", "b"), up("c", "d")), 0, 1) == "b"
+    assert component(0, 1)(up(up("a", "b"), up("c", "d"))) == "b"
