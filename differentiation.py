9e3a1127397307cbe8cc46b227a44dd54b151b52
James Tauber <jtauber@jtauber.com>
2013-09-04 01:52:55 -0400

    fixed pylint errors in differentation

--- differentiation.py
+++ differentiation.py
@@ -5,27 +5,37 @@ see ``differentiation.rst`` for an explanation.
 import numbers
 
 from simplification import simplify
-from symbolic import *
+from symbolic import Num, Sym, Add, Mul, Pow
 
 
-def D(expr, wrt):
+def D(expr, wrt): # pylint: disable=C0103
+    """
+    Differentiate ``expr`` with respect to ``wrt``.
+    """
     if isinstance(expr, numbers.Number):
-        return 0
+        derivative = 0
     elif isinstance(expr, Num):
-        return 0
+        derivative = 0
     elif isinstance(expr, Sym):
         if expr == wrt:
-            return 1
+            derivative = 1
         else:
-            return 0
+            derivative = 0
     elif isinstance(expr, Add):
-        return simplify(D(expr.arg1, wrt) + D(expr.arg2, wrt))
+        derivative = simplify(D(expr.arg1, wrt) + D(expr.arg2, wrt))
     elif isinstance(expr, Mul):
-        return simplify(D(expr.arg1, wrt) * expr.arg2 + D(expr.arg2, wrt) * expr.arg1)
+        derivative = simplify(
+            D(expr.arg1, wrt) * expr.arg2 +
+            D(expr.arg2, wrt) * expr.arg1
+        )
     elif isinstance(expr, Pow):
         if expr.arg1 == wrt and isinstance(expr.arg2, Num):
-            return simplify(expr.arg2 * (expr.arg1 ** (expr.arg2 - 1)))
+            derivative = simplify(expr.arg2 * (expr.arg1 ** (expr.arg2 - 1)))
         else:
-            raise NotImplementedError("haven't implemented differentiation of powers in the general case")
+            raise NotImplementedError(
+                "haven't implemented differentiation of powers in general case"
+            )
     else:
         raise TypeError("can't handle {}".format(type(expr)))
+
+    return derivative
